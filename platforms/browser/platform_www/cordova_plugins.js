d75961efe4e5ab5a9ca482c6fbff3df71d68ba2f
siematypie <mkkc12@gmail.com>
2017-08-27 16:49:56 +0200

    Add new files

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,50 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-downloadmanager/www/DownloadManager.js",
+        "id": "cordova-plugin-downloadmanager.DownloadManager",
+        "pluginId": "cordova-plugin-downloadmanager",
+        "clobbers": [
+            "cordova.plugins.DownloadManager"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.6",
+    "cordova-plugin-dialogs": "1.3.3",
+    "cordova-plugin-downloadmanager": "0.0.1"
+}
+// BOTTOM OF METADATA
+});
